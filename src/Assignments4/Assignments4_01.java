e8521a00e6ce66fefb6b0fb81d35f4cbbd72ae5f
radhacoolk212 <radhacool.k212@gmail.com>
2025-03-09 23:13:18 +0530

    Assignments

--- src/Assignments4/Assignments4_01.java
+++ src/Assignments4/Assignments4_01.java
@@ -6,42 +6,41 @@ public class Assignments4_01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		HashMap<String,Object> data1 = new HashMap<String,Object>();
-		data1.put("EmployeeId","E001");
-		data1.put("Name","Alice Green");
-		data1.put("Age",30);
-		data1.put("Gender","Female");
-		data1.put("Department","Engineering");
-		data1.put("Position","SoftwareEngineer");
-		data1.put("Salery","75,00");
-		data1.put("Email","alice@example.com");
-		data1.put("ContactNumber",9876543213l);
+
+		HashMap<String, Object> data1 = new HashMap<String, Object>();
+		data1.put("EmployeeId", "E001");
+		data1.put("Name", "Alice Green");
+		data1.put("Age", 30);
+		data1.put("Gender", "Female");
+		data1.put("Department", "Engineering");
+		data1.put("Position", "SoftwareEngineer");
+		data1.put("Salery", "75,00");
+		data1.put("Email", "alice@example.com");
+		data1.put("ContactNumber", 9876543213l);
 		//
-		HashMap<String,Object> data2 = new HashMap<String,Object>();
-		data2.put("EmployeeId","E002");
-		data2.put("Name","BobJohnson");
-		data2.put("Age",35);
-		data2.put("Gender","male");
-		data2.put("Department","Marketing");
-		data2.put("Position","MarketingManager");
-		data2.put("Salery","85,00");
-		data2.put("Email","bob@example.com");
-		data2.put("ContactNumber",9876543214l);
+		HashMap<String, Object> data2 = new HashMap<String, Object>();
+		data2.put("EmployeeId", "E002");
+		data2.put("Name", "BobJohnson");
+		data2.put("Age", 35);
+		data2.put("Gender", "male");
+		data2.put("Department", "Marketing");
+		data2.put("Position", "MarketingManager");
+		data2.put("Salery", "85,00");
+		data2.put("Email", "bob@example.com");
+		data2.put("ContactNumber", 9876543214l);
 		//
-		HashMap<String,Object> data3 = new HashMap<String,Object>();
-		data3.put("EmployeeId","E003");
-		data3.put("Name","CarolWhite");
-		data3.put("Age",28);
-		data3.put("Gender","FEmale");
-		data3.put("Department","Sales");
-		data3.put("Position","SalesExecutive");
-		data3.put("Salery","65,00");
-		data3.put("Email","carol@example.com");
-		data3.put("ContactNumber",9876543215l);
-		
+		HashMap<String, Object> data3 = new HashMap<String, Object>();
+		data3.put("EmployeeId", "E003");
+		data3.put("Name", "CarolWhite");
+		data3.put("Age", 28);
+		data3.put("Gender", "FEmale");
+		data3.put("Department", "Sales");
+		data3.put("Position", "SalesExecutive");
+		data3.put("Salery", "65,00");
+		data3.put("Email", "carol@example.com");
+		data3.put("ContactNumber", 9876543215l);
+
 		System.out.println(data2.get("Email"));
-		
 
 	}
 
--- src/Assignments5/Assignments5_1.java
+++ src/Assignments5/Assignments5_1.java
@@ -12,17 +12,19 @@ public class Assignments5_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//*********1. Create a list of the top 5 most populated countries in the world and print 2nd Country
+		// *********1. Create a list of the top 5 most populated countries in the world
+		// and print 2nd Country
 		System.out.println("*********************top 5 most populated countries*************************");
-		List<String> PopulatedCountries =new ArrayList<String>();
+		List<String> PopulatedCountries = new ArrayList<String>();
 		PopulatedCountries.add("India");
 		PopulatedCountries.add("China");
 		PopulatedCountries.add("United States");
 		PopulatedCountries.add("Indonesia");
 		PopulatedCountries.add("Pakistan");
-		
+
 		System.out.println(PopulatedCountries.get(1));
-	//	2. Create a set of the top 10 most visited tourist attractions in the world and print out its
+		// 2. Create a set of the top 10 most visited tourist attractions in the world
+		// and print out its
 		System.out.println("*********************top 10 most visited tourist attractions*************************");
 		Set<String> Top10 = new HashSet<String>();
 		Top10.add("Taj Mahal");
@@ -35,44 +37,55 @@ public class Assignments5_1 {
 		Top10.add("Anne Frank House");
 		Top10.add("Gardens by the Bay");
 		Top10.add("Duomo of Milan");
-		
+
 		System.out.println(Top10.size());
-		
-	//3.Create a map of the 5 largest cities in the United States and their populations.	
-		System.out.println("*********************the 5 largest cities in the United States************************");	
-		Map<String,String> LargestCities = new HashMap<String,String>();
+
+		// 3.Create a map of the 5 largest cities in the United States and their
+		// populations.
+		System.out.println("*********************the 5 largest cities in the United States************************");
+		Map<String, String> LargestCities = new HashMap<String, String>();
 		LargestCities.put("New York", "8,335,897");
 		LargestCities.put("California", "3,822,238");
 		LargestCities.put("Chicago", "2,665,039");
 		LargestCities.put("Houston", "2,302,878");
 		LargestCities.put("Phoenix", " 1,644,409");
-		
+
 		System.out.println(LargestCities);
-		
-		//4.4. Create an array of 10 random integers and print out the sum of 3rd and 5th Value.
+
+		// 4.4. Create an array of 10 random integers and print out the sum of 3rd and
+		// 5th Value.
 		System.out.println("*********************10 random integers************************");
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		//5. Create a list of the top 5 highest-grossing movies of all time and print out the third movieon the list.
-		
+
+		int integers[] = new int[10];
+		integers[0] = 1;
+		integers[1] = 2;
+		integers[2] = 3;
+		integers[3] = 4;
+		integers[4] = 5;
+		integers[5] = 6;
+		integers[6] = 7;
+		integers[7] = 8;
+		integers[8] = 9;
+		integers[9] = 10;
+
+		int sum;
+		sum = integers[2] + integers[4];
+		System.out.println("sum of 3rd and 5th Value:" + sum);
+
+		// 5. Create a list of the top 5 highest-grossing movies of all time and print
+		// out the third movieon the list.
+
 		System.out.println("*********************the top 5 highest-grossing movies************************");
 
-		List<String> movies= new ArrayList<String>();
+		List<String> movies = new ArrayList<String>();
 		movies.add("Avatar");
 		movies.add("Avengers: Endgame");
 		movies.add("Avatar: The Way of Water");
 		movies.add("Titanic");
 		movies.add("Star Wars: The Force Awakens");
-		
+
 		System.out.println(movies.get(2));
-		
+
 	}
 
 }
