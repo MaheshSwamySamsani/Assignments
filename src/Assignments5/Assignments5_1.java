132589b951bcb923f423c91d732b0134a00296df
radhacoolk212 <radhacool.k212@gmail.com>
2025-03-10 08:32:45 +0530

    Assignments

--- src/Assignments5/Assignments5_1.java
+++ src/Assignments5/Assignments5_1.java
@@ -15,42 +15,42 @@ public class Assignments5_1 {
 		// *********1. Create a list of the top 5 most populated countries in the world
 		// and print 2nd Country
 		System.out.println("*********************top 5 most populated countries*************************");
-		List<String> PopulatedCountries = new ArrayList<String>();
-		PopulatedCountries.add("India");
-		PopulatedCountries.add("China");
-		PopulatedCountries.add("United States");
-		PopulatedCountries.add("Indonesia");
-		PopulatedCountries.add("Pakistan");
+		List<String> populatedCountries = new ArrayList<String>();
+		populatedCountries.add("India");
+		populatedCountries.add("China");
+		populatedCountries.add("United States");
+		populatedCountries.add("Indonesia");
+		populatedCountries.add("Pakistan");
 
-		System.out.println(PopulatedCountries.get(1));
+		System.out.println(populatedCountries.get(1));
 		// 2. Create a set of the top 10 most visited tourist attractions in the world
 		// and print out its
 		System.out.println("*********************top 10 most visited tourist attractions*************************");
-		Set<String> Top10 = new HashSet<String>();
-		Top10.add("Taj Mahal");
-		Top10.add("Eiffel Tower");
-		Top10.add("Basilica de la Sagrada Familia");
-		Top10.add("Taj MahalGreat Smoky Mountains National Park");
-		Top10.add("Empire State Building");
-		Top10.add("The Louvre");
-		Top10.add("Colosseum");
-		Top10.add("Anne Frank House");
-		Top10.add("Gardens by the Bay");
-		Top10.add("Duomo of Milan");
+		Set<String> top10 = new HashSet<String>();
+		top10.add("Taj Mahal");
+		top10.add("Eiffel Tower");
+		top10.add("Basilica de la Sagrada Familia");
+		top10.add("Taj MahalGreat Smoky Mountains National Park");
+		top10.add("Empire State Building");
+		top10.add("The Louvre");
+		top10.add("Colosseum");
+		top10.add("Anne Frank House");
+		top10.add("Gardens by the Bay");
+		top10.add("Duomo of Milan");
 
-		System.out.println(Top10.size());
+		System.out.println(top10.size());
 
 		// 3.Create a map of the 5 largest cities in the United States and their
 		// populations.
 		System.out.println("*********************the 5 largest cities in the United States************************");
-		Map<String, String> LargestCities = new HashMap<String, String>();
-		LargestCities.put("New York", "8,335,897");
-		LargestCities.put("California", "3,822,238");
-		LargestCities.put("Chicago", "2,665,039");
-		LargestCities.put("Houston", "2,302,878");
-		LargestCities.put("Phoenix", " 1,644,409");
+		Map<String, String> largestCities = new HashMap<String, String>();
+		largestCities.put("New York", "8,335,897");
+		largestCities.put("California", "3,822,238");
+		largestCities.put("Chicago", "2,665,039");
+		largestCities.put("Houston", "2,302,878");
+		largestCities.put("Phoenix", " 1,644,409");
 
-		System.out.println(LargestCities);
+		System.out.println(largestCities);
 
 		// 4.4. Create an array of 10 random integers and print out the sum of 3rd and
 		// 5th Value.
