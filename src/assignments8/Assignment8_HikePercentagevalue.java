0bf8f5599f92b97e69c9fdb16f06348df7341242
MaheshSwamySamsani <samsanimaheshswamy@gmail.com>
2025-04-02 10:42:56 +0530

    Assignments

--- src/assignments8/Assignment8_HikePercentagevalue.java
+++ src/assignments8/Assignment8_HikePercentagevalue.java
@@ -3,7 +3,20 @@ package assignments8;
 public class Assignment8_HikePercentagevalue {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		/*
+		 * Employees with Experience >= 5 Years get extra Reward of 5000;
+		 * 
+		 * There are 5 Employee records. Now Calculate the Hike percentage of each
+		 * Employee and store the Values in Map with EmployeeName and
+		 * HikePercentagevalue and Print them.
+		 * 
+		 * Hike = ( Base Salary * variable pay % ) + Bonus+Reward ; Hike % = Hike / Base
+		 * Salary .
+		 */
+		
+		
+		
 
 	}
 
---
+++ src/assignments999/Mahesh1.java
@@ -0,0 +1,10 @@
+package assignments999;
+
+public class Mahesh1 {
+
+	public static void main(String[] args) {
+		//Swap two numbers without using a third variable.
+
+	}
+
+}
